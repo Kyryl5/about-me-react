22eb73fcf0489c0873aef69e1c1507206ae3f718
Kyryl Savytskyi <kirill_savitsky56@gmail.com>
2022-11-02 13:02:37 +0200

    photo host

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Avatar from '@mui/material/Avatar'
-import Photo from '../../../public/img/photo.jpeg'
+// import Photo from '../../../public/img/photo.jpeg'
 
 function Header() {
   return (
@@ -17,7 +17,7 @@ function Header() {
           <Box sx={{ flexGrow: 0, p: 1 }}>
             <Avatar
               alt="user-photo"
-              src={Photo}
+              src={'https://i.postimg.cc/XYTY9ncq/photo.jpg'}
               sx={{ width: 100, height: 100 }}
             />
           </Box>
