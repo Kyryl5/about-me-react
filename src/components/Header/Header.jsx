4cbeb5f29bb0cc5a9e74cfb94649256ab78538aa
Kyryl Savytskyi <kirill_savitsky56@gmail.com>
2022-11-02 12:53:54 +0200

    photo 4

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Avatar from '@mui/material/Avatar'
+import Photo from '../../../public/img/photo.jpeg'
 
 function Header() {
   return (
@@ -16,7 +17,7 @@ function Header() {
           <Box sx={{ flexGrow: 0, p: 1 }}>
             <Avatar
               alt="user-photo"
-              src="../img/photo.jpeg"
+              src={Photo}
               sx={{ width: 100, height: 100 }}
             />
           </Box>
